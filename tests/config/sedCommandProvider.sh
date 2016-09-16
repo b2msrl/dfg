e97c68d03da39db09cb3d9bb549d087d57ed525e
alevento <a@a.it>
2016-09-16 11:55:07 +0200

    Substitute

--- tests/config/sedCommandProvider.sh
+++ tests/config/sedCommandProvider.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i "s/'providers' => \[/'providers' => \[ @@@namespacevendor\\\\@@@namespacepackage_name\\\\@@@providerpackage_nameServiceProvider::class,/g" ./config/app.php
+sed -i "s/'providers' => \[/'providers' => \[ Padosoft\\\\Dfg\\\\DfgServiceProvider::class,/g" ./config/app.php
